276ee1fc7cc733f2f77d0206935e3b851771eba2
palak1303 <137207438+palak1303@users.noreply.github.com>
2024-03-23 11:34:57 +0530

    Update sudoku.java

--- sudoku.java
+++ sudoku.java
@@ -48,14 +48,14 @@ public class sudoku {
                 if(sudokuSolver(sudoku,nextRow,nextCol)){    //soln exist
                     return true;
                 }
-            }
+                sudoku[row][col] =0;            }
         }
         return false;
     }
     public static void printsudoku(int sudoku[][]){
         for(int i = 0; i<9;i++){
             for(int j= 0;j<9;j++){
-                System.out.println(sudoku[i][j] + "");
+                System.out.print(sudoku[i][j] + " ");
             }
             System.out.println();
         }
@@ -63,15 +63,15 @@ public class sudoku {
    
     public static void main(String args[]){
         int sudoku[][] = {
-    {0, 8, 8, 0, 0, 0, 0, 0, 0},
-    {4, 9, 8, 1, 5, 7, 0, 0, 2},
-    {0, 0, 3, 0, 0, 4, 1, 9, 0},
-    {1, 8, 5, 0, 6, 0, 0, 2, 0},
-    {0, 0, 0, 0, 2, 0, 0, 6, 0},
-    {9, 6, 0, 4, 8, 5, 3, 0, 0},
-    {0, 3, 0, 0, 7, 2, 0, 0, 4},
-    {0, 4, 9, 8, 3, 0, 0, 5, 7},
-    {8, 2, 7, 8, 8, 9, 0, 1, 3} };
+   {3, 0, 6, 5, 0, 8, 4, 0, 0},
+{5, 2, 0, 0, 0, 0, 0, 0, 0},
+{0, 8, 7, 0, 0, 0, 0, 3, 1},
+{0, 0, 3, 0, 1, 0, 0, 8, 0},
+{9, 0, 0, 8, 6, 3, 0, 0, 5},
+{0, 5, 0, 0, 9, 0, 6, 0, 0}, 
+{1, 3, 0, 0, 0, 0, 2, 5, 0},
+{0, 0, 0, 0, 0, 0, 0, 7, 4},
+{0, 0, 5, 2, 0, 6, 3, 0, 0}  };
 
     if(sudokuSolver(sudoku,0,0)){
         System.out.println("solution exist");
