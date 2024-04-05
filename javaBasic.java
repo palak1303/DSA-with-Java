6f40bfa1b0c2c4ccf99f027db4118a5f07bd4a26
palak1303 <palakrehan862@gmail.com>
2024-04-05 22:01:46 +0530

    javaBasic- conversion,promotion & basicQues

---
+++ javaBasic.java
@@ -0,0 +1,98 @@
+    import java.util.*;
+
+// public class javaBasic{} 
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+
+        // TYPE CONVERSION
+
+        
+        // float num = sc.nextInt();
+        // System.out.print(num);
+        // int a = 25;
+        // long b = a;
+        // System.out.print(b);
+
+        // TYPE CASTING
+        // float a = 25.5f;
+        // int b = (int)a;
+        // System.out.println(b);
+        // char ch = 'a';
+        // char ch2 = 'A';
+        // int num = ch;
+        // int num2 = ch2;
+        // System.out.println(num);
+        // System.out.println(num2);
+
+//      TYPE PROMOTION
+        // char a = 'a';
+        // char b = 'b';
+        // System.out.println((int)a);
+        // System.out.println((int)b);
+        // System.out.println((b-a));
+        
+        // example2
+        // byte , short , char --->int in exp
+        // short a = 5;
+        // byte b = 25;
+        // char c = 'c';
+        // byte bt = a + b + c;
+        // System.out.println(bt);
+        
+        //  example 3
+        // long ,float ,doblue --->bigest one resp;
+        // int a = 10;
+        // float b  = 20.34f;
+        // long c = 25;
+        // double d = 30;
+        // double ans  = a + b + c + d;
+        // System.out.println(ans);
+ 
+    //    example
+        // byte b = 5;
+        // byte a = b*2;  wrong
+        // byte a = (byte)(b*2); righht
+        // System.out.println(a);
+
+    // }
+    
+// }
+
+// PRATCICE SET
+ public class javaBasic {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+        
+        // QUESTION1 --->AVG OF 3
+        // int a  =  sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int avg = (a + b + c)/3;
+        // System.out.print(avg);
+
+        // QUESTION2 --->AREA OF SQUARE
+        // int side =  sc.nextInt();
+        // float area = side*2;
+        // System.out.println(area);
+
+        // QUESTION3 ---->TOTAL BILL OF 3ITEMS
+        // float pencil = sc.nextInt();
+        // float eraser = sc.nextInt();
+        // float pen = sc.nextInt();
+        // float total = (pencil + pen + eraser);
+        // System.out.println(total);
+        
+    // byte b = 4
+    // char c = 'a';
+    // short s = 512
+    // int i = 1000 ;
+    // float f = 3.14f
+    // double d = 99.9954
+    // result = (f * b) + (i % c) - (d * s);
+    
+    int $ = 24;
+    System.out.println($);
+
+    }
+}
+
