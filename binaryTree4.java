1201dc38edca8971cfd0ae83e87b950854b43159
palak1303 <palakrehan862@gmail.com>
2024-04-30 23:43:37 +0530

    Kth level, lastCommonAncestor(approach1&2)

---
+++ binaryTree4.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+public class binaryTree4 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+    
+        Node( int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+    
+        }
+   }
+
+//    Kth level
+   public static void KLevel(Node root, int level, int K){
+    if(root == null){
+        return;
+    }
+
+    if(level == K){
+        System.out.println(root.data+"");
+        return;
+    }
+    KLevel(root.left,level+1,K);
+    KLevel(root.right,level+1,K);
+
+   }
+
+
+//    last common amcestor -O(n)
+public static boolean getPath(Node root, int n, ArrayList<Node> path){
+    if(root == null){
+        return false;
+    }
+    path.add(root);
+
+    if( root.data ==n){
+        return true;
+    }
+
+    boolean foundLeft = getPath(root.left, n, path);
+    boolean foundRight = getPath(root.right, n, path);
+
+    if( foundLeft || foundRight){
+        return true;
+    }
+    path.remove(path.size()-1);
+    return false;
+   }
+
+
+public static Node lca(Node root ,int n1, int n2){
+    ArrayList<Node> path1= new ArrayList<>();
+    ArrayList<Node> path2= new ArrayList<>();
+
+    getPath(root,n1, path1);
+    getPath(root,n2, path2);
+
+    // last commmon ancestor
+    int i = 0;
+    for(; i< path1.size() && i< path2.size() ; i++){
+        if( path1.get(i) != path2.get(i)){
+            break;
+        }
+    }
+    //last equal node -> i-1th
+    Node lca = path1.get(i-1);
+    return lca;
+}
+
+
+// approach 2
+public static Node lca2( Node root, int n1, int n2){
+
+    if( root == null || root.data == n1 || root.data == n2){
+        return root;
+    }
+
+    Node leftLca = lca2(root.left,n1,n2);
+    Node rightLca = lca2(root.right,n1,n2);
+
+
+    // left lca = val righrLca = null
+    if( rightLca == null){
+        return leftLca;
+    }
+    if( leftLca == null){
+        return rightLca;
+    }
+    return root;
+
+}
+    public static void main(String[] args) {
+    /*
+             1
+           /  \
+          2    3
+        /  \  / \
+       4    5 6  7
+
+    */ 
+
+    Node root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
+
+    // int K = 3;
+    // KLevel(root, 1,K);
+        int n1 = 4, n2 = 6;
+        System.out.print(lca2(root, n1,n2).data);
+
+}
+}
