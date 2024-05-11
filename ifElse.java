5f51d8e5533c27a017c50eb9843cb67c823d447f
palak1303 <palakrehan862@gmail.com>
2024-05-11 22:21:55 +0530

    ifEles, elseIf, switch , ternary

---
+++ ifElse.java
@@ -0,0 +1,117 @@
+
+    import java.util.*;
+    public class ifElse {
+        public static void main( String args[]){
+    
+  // IF ELSE
+    
+        //  int age = 16;
+        //  if(age>= 18){
+        //     System.out.println("adult : drive,votee");
+        //  }
+        //  if (age>13 && age<18){
+        //     System.out.println("teenager");
+        //  }
+        //  else {
+        //      System.out.println("not adult");
+        //  }
+        
+        // //  LARGER OF 2
+
+        // int a = 1;
+        // int b = 3;
+        // if(a>b)
+        // {
+        //     System.out.println(" A is grearter");
+        // }
+        // else
+        // {   
+        //     System.out.println(" b is grearter");
+        // }
+    
+        // ODD OR EVEN
+
+        // Scanner sc =  new Scanner(System.in);
+        // int n = sc.nextInt();
+        // if(n % 2 == 0){
+        //     System.out.println("even ");
+        //   }
+        // else{
+        //     System.out.println(" odd ");
+        // }
+    
+ // ELSE IF 
+    
+        // int age = 13;
+        // if (age <= 18){
+        //   System.out.println("adult");
+        // }
+        // else if (age>=13 && age < 18){
+        //    System.out.println("teenager"); 
+        // }
+        // else{
+        //     System.out.println("not adult");
+        // }
+          
+        //      INCOME TAX CALCULATOR
+
+        // Scanner sc = new Scanner(System.in);
+        // int income = sc.nextInt();
+        // int tax ;
+        // if( income < 500000){
+        //    tax = 0;
+        // }
+        // else if (income >500000 && income <1000000){
+        //     tax = (int) (income * 0.2);
+        // }
+        // else{
+        //     tax = ( int)(income * 0.3);
+        // }
+        // System.out.println("your tax is " + tax );
+    
+
+    //       LARGEST OF 3
+
+       int a = 1;
+       int b = 3;
+       int c = 6;
+       if (( a> b ) && ( a > c )){
+       System.out.println(" a is largest");
+       }
+       else if ( b>c){
+        System.out.println(" b is largest");
+       }
+       else{
+        System.out.println(" c is largest");
+       }
+       
+       
+//    TERNARY OPERATOR 
+    
+    //  int N = 54;
+    // String type = (( N % 2) == 0) ? "even": "odd";
+    // System.out.println(type);
+    
+    // int marks = 50;
+    // String typ = ((marks >= 33))? "pass": "fail";
+    // System.out.println(type);
+
+
+    
+ //        SWITCH STATEMEN 
+    
+    // int n = 2;    a can be writen instead of 1
+    // switch( n ){
+    //     case 1:System.out.println("samosa");
+    //     break;
+    //     case 2:System.out.println("momo");
+    //     break;
+    //     case 3:System.out.println("burger");
+    //     break;
+    //     default:System.out.println("we wake up");
+    // }
+    
+    }
+    }
+     
+
