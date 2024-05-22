2271675655d6ac28fca258688d68687007f857c9
palak1303 <palakrehan862@gmail.com>
2024-05-23 01:52:43 +0530

    implementation

---
+++ graph1.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class graph1 {
+    static class Edge{
+        int src;int dest;
+        int wt;
+
+        public Edge(int s, int d, int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+    public static void main(String args[]){
+
+        int V = 5;
+        ArrayList<Edge>[] graph = new ArrayList[V];   //null
+        
+        for( int i = 0; i<V;i++){
+            graph[i] = new ArrayList<>();
+        }
+            // 0vertex
+            graph[0].add(new Edge(0,1,5));
+            // 1vertex
+            graph[1].add(new Edge(1,0,5));
+            graph[1].add(new Edge(1,3,3));
+            graph[1].add(new Edge(1,2,1));
+            // 2vertex
+            graph[2].add(new Edge(2,1,1));
+            graph[2].add(new Edge(2,3,1));
+            graph[2].add(new Edge(2,4,4));
+            //3vertex
+            graph[3].add(new Edge(3,1,3));
+            graph[3].add(new Edge(3,2,1));
+            //4vertex
+            graph[4].add(new Edge(4,2,2));
+
+            // 2's neighbour
+            for( int i= 0 ; i<graph[2].size();i++){
+                Edge e = graph[2].get(i);
+                System.out.println(e.dest);
+            }
+        
+        
+    }
+}
