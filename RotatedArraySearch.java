658cb39bd18262a838bf226fa6b543f20f9d891c
palak1303 <palakrehan862@gmail.com>
2024-06-12 11:21:39 +0530

    RotatedArraySearch

--- RotatedArraySearch.java
+++ RotatedArraySearch.java
@@ -20,25 +20,25 @@ public class rotatedsorredarr {
              return mid;
          }
 
-         if(nums[s] <= nums[mid]){
-             if(target>=nums[s] && target < nums[mid]){
-                 e=mid-1;
-             }else{
-                 s=mid+1;
-             }
-         }else{
-             if(nums[mid]<target && target <= nums[e]){
-                 s=mid+1;
-             }else{
-                 e=mid-1;
-             }
-         }
+        if(nums[s] <= nums[mid]){
+            if(target >=nums[s] && target < nums[mid]){
+                e=mid-1;
+            }else{
+                s=mid+1;
+            }
+        }else{
+            if(nums[mid]<target && target <= nums[e]){
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
         }
+    }
         return -1;
     }
     public static void main(String[] args) {
         int arr[]={4,5,6,7,0,1,2};
-        System.out.println(search(arr,4)); 
+        System.out.println(search(arr,7)); 
     }
     
 }
