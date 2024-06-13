0c74a321f4abcb43b1889eb9b5628dce6337835f
palak1303 <palakrehan862@gmail.com>
2024-06-13 19:37:00 +0530

    isValid ,duplicateParenthsis

--- stack3.java
+++ stack3.java
@@ -9,21 +9,20 @@ public class stack3 {
             if( ch =='(' || ch == '{' || ch == '[' ){
                 s.push(ch);
             }else{
-                //closing
-                if(s.isEmpty()){
-                    return false;
-                }
-                if((s.peek()=='('&& ch ==')')
-                || (s.peek() == '{' && ch=='}')
-                || (s.peek() == '[' && ch == ']')){
-                    s.pop();
+            //closing
+            if(s.isEmpty()){
+                return false;
+            }
+            if((s.peek()=='('&& ch ==')')
+            || (s.peek() == '{' && ch=='}')
+            || (s.peek() == '[' && ch == ']')){
+                s.pop();
 
-                }else{
-                    return false;
-                }
-
+            }else{
+                return false;
             }
         }
+    }
         if(s.isEmpty()){
             return true;
         }else{
@@ -33,31 +32,31 @@ public class stack3 {
 
 
     //duplicate parenthesis
-// public static boolean isDuplicate(String str){
-//     Stack<Character> s = new Stack<>();
+public static boolean isDuplicate(String str){
+    Stack<Character> s = new Stack<>();
 
-//     for( int i = 0 ; i< str.length();i++){
-//         char ch = str.charAt(i);
+    for( int i = 0 ; i< str.length();i++){
+        char ch = str.charAt(i);
 
-//         //closing
-//         if( ch ==')'){
-//             int count = 0;
-//             while( s.peek() != '('){
-//                 s.pop();
-//                 count++;
-//             }
-//             if(count < 1){
-//                 return true;
-//             }else{
-//                 s.pop();
-//             }
-//         }else{
-//             //opening
-//             s.push(ch);
-//         }
-//     }
-//     return false;
-// }
+        //closing
+        if( ch ==')'){
+            int count = 0;
+            while( s.peek() != '('){
+                s.pop();
+                count++;
+            }
+            if(count < 1){
+                return true;
+            }else{
+                s.pop();
+            }
+        }else{
+            //opening
+            s.push(ch);
+        }
+    }
+    return false;
+}
 
 
 public static void main(String args[]){
@@ -65,7 +64,7 @@ public static void main(String args[]){
     System.out.println(isValid(str));
     // String str = "((a+b))";
     // String str2 = "(a+b)";
-    //     System.out.println(isDuplicate(str));
+    // System.out.println(isDuplicate(str));
 
 }
 }
